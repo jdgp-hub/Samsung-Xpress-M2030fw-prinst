ac507bf2e7f0fb17ead03b0cedafdad4a40dcfeb
JanGray <kittsbooks@comcast.net>
2021-08-11 01:32:24 -0500

    Initialize xo-ts

---
+++ source/globals.d.ts
@@ -0,0 +1,14 @@
+declare const browser: AnyObject;
+
+type AnyObject = {[key: string]: any};
+
+declare module 'select-dom' {
+	import 'select-dom';
+
+	function select(selector: string, parent?: any): any | null;
+	namespace select {
+		export function exists(selector: string, parent?: Element): boolean;
+		export function all(selector: string, parent?: Element | Element[] | NodeList | DocumentFragment): any[];
+	}
+	export = select;
+}
